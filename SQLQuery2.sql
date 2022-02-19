1e6cc568d4e6b86d5a14c7fd3e91e3151ff4c143
rasipriyanka <priyankapendela111@gmail.com>
2022-02-19 19:00:45 +0530

    create table for addressBook

--- SQLQuery2.sql
+++ SQLQuery2.sql
@@ -1,1 +1,12 @@
 create database AddressBookService;
+create table AddressBook(
+Id int IDENTITY(1,1) PRIMARY KEY,
+FirstName varchar(200),
+LastName varchar(200),
+Address varchar(200),
+City varchar(200),
+State varchar(200),
+Zip bigint,
+PhoneNumber bigint,
+Email varchar(200),
+);
